ae3acfe951618e72d113317cad4707fd09ab0b00
Robin Böckmann <hobba11@msn.com>
2016-01-05 20:18:24 +0100

    my code for no 6

---
+++ grp27_ueb06/src/grp27_ueb06/Grp27_ueb06.java
@@ -0,0 +1,23 @@
+/*
+ * Um die Parkreihen in einem Parkhaus künftig effizienter belegen zu können, 
+ * soll ein Parkroboter für das automatische Einparken der Autos eingesetzt 
+ * werden können. 
+ * Es soll eine Klasse Row erstellt werden, die Informationen über ihre 
+ * Länge und ihre Lücken bereitstellt.
+ */
+package grp27_ueb06;
+
+/**
+ *
+ * @author Gerit
+ */
+public class Grp27_ueb06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
